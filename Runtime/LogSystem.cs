64f030671afcd891cbf7b4eefc1ca5e3777d310c
howard <howard1689@gmail.com>
2025-01-22 11:13:41 +0800

    Update 添加 LogSystem

---
+++ Runtime/LogSystem.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Howard.Tools
+{
+    /// <summary>
+    /// 輸出系統
+    /// </summary>
+    public class LogSystem : MonoBehaviour
+    {
+        /// <summary>
+        /// 輸出系統
+        /// </summary>
+        /// <param name="message">輸出內容</param>
+        /// <param name="color">顏色</param>
+        public static void Log(string message, string color = "#fff")
+        {
+            Debug.Log($"<color={color}>{message}</color>");
+        }
+    }
+}
